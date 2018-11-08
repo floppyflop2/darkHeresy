ecb18bc83d83c067f6cbfb1b7511ed1c8112fa45
floppyflop2 <floppy007_flo@hotmail.com>
2018-11-08 16:41:42 +0100

    all back

--- darkHeresyModel/Program.cs
+++ darkHeresyModel/Program.cs
@@ -10,6 +10,22 @@ namespace darkHeresyModel
     {
         static void Main(string[] args)
         {
+            using (var db = new DarkHeresyModel())
+            {
+                //db.PersonalInfos.Add(new PersonalInfo { Id =1 , Name="toto", PersonalGender=Gender.UNDEFINED,
+                //    Age=12, Description="blablabalb", Build="huge", Complexion="wow", Hair="brown" });
+                db.Tests.Add(new Test { TestName="toto" });
+                db.SaveChanges();
+
+                foreach (var pi in db.Tests)
+                {
+                    Console.WriteLine(pi.TestName);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
+    
 }
